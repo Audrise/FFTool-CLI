c510cbd7212e81a54ef6801af59f59bd7916f52f
Audrise <171641763+Audrise@users.noreply.github.com>
2026-07-21 21:25:06 +0700

    Update main.py
    Co-Authored-By: nullptrdev1 <144085465+nullptrdev1@users.noreply.github.com>

--- main.py
+++ main.py
@@ -686,7 +686,7 @@ def main():
                 System.Clear()
 
             elif choice == "0":
-                print(error(f"{red}Exitting{res}"))
+                print(error(f"{red}Exit.{res}"))
                 sleep(0.5)
                 System.Clear()
                 break
